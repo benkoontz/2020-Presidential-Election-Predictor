9831212a1d990cd9fcbb7543ecbdc09eacc45703
benkoontz <36342241+benkoontz@users.noreply.github.com>
2020-07-04 14:58:24 -0700

    Create Republican.java

---
+++ Republican.java
@@ -0,0 +1,12 @@
+
+public class Republican extends President{
+	
+	int States = 50;
+	String President = "Donald Trump";
+	int total_votes = 306;
+	
+	public int ElectoralVote() {
+		return 306;
+	}
+	
+}
